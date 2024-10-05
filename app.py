a31a70dd69294f0f3448a01f88133887f6d9019c
TheRealMopcornPuncher <m0pcornpuncher@gmail.com>
2024-10-05 08:59:23 -0500

    Add apology.html
    Adds an apology page to handle any errors

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
 @app.route("/")
 def index():
     return render_template("index.html")
